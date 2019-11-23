731e9a943a3aba54993e002856d85311fd86dd6a
Annie Decker <anniedecker@ou.edu>
2019-11-22 19:13:34 -0600

    basic seat generator

---
+++ RandomSeatAssigner/RandomSeatAssigner/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RandomSeatAssigner
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnFile_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            
+            var result = dlg.ShowDialog();
+            
+            txtFile.Text = dlg.FileName;
+        }
+
+        private void BtnAssign_Click(object sender, RoutedEventArgs e)
+        {
+            if (File.Exists(txtFile.Text) == true)
+
+            {
+                var lines = File.ReadAllLines(txtFile.Text);
+                List<int> randomNumbers = new List<int>();
+                Random random = new Random();
+                int row = 0;
+                
+                for (int i = 0; i < lines.Length; i++)
+
+                {
+                    int number;
+                    var line = lines[i];
+                    var column = line.Split(',');
+                    string name = column[0];
+
+                    do
+                    {
+                        number = random.Next(1, lines.Length + 1);
+                        
+                    } while (randomNumbers.Contains(number));
+                    
+                    if (number > 1 && number <= 12)
+                        row = 1;
+                    else if (number > 12 && number <= 25)
+                        row = 2;
+                    else if (number > 25 && number <= 38)
+                        row = 3;
+                    else
+                        row = 4;
+                    lstSeats.Items.Add($"{name:B} -  Row {row}, Computer {number}");
+                    randomNumbers.Add(number);
+                }
+            }
+        }
+    }
+}
